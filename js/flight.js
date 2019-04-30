e3a785e92716a37c6a7c39ef6284d9b9894c5437
Vlad N <vlad.neri@gmail.com>
2019-04-30 11:39:19 -0700

    draft

--- js/flight.js
+++ js/flight.js
@@ -1,57 +1,40 @@
-  //  https://api.flightstats.com/flex/flightstatus/rest/v2/json/flight/status/AA/1659/arr/2019/4/27?appId=240766ef&appKey=c171a1e862d6550fa4f0ae7d6dc00c5a&utc=false
-// Set Up Variables
-
+ 
+// set up variables
   var carrier = "";
   var flightNum = "";
   var date = "";
-
-  //  https://api.flightstats.com/flex/flightstatus/rest/v2/json/flight/status/{airline}/{flightNumber}/arr/{year}/{month}/{day}?appId=240766ef&appKey=c171a1e862d6550fa4f0ae7d6dc00c5a&utc=false
-  // https://api.flightstats.com/flex/schedules/rest/v1/json/flight/nw/1070/departing/2019/4/29?appId=240766ef&appKey=58e50827114a43dca4fe80587bcc8f37"
 
   var baseURL = "https://api.flightstats.com/flex/flightstatus/rest/v2/jsonp/flight/status/";
   var appId = "?appId=240766ef"
   var apiKey = "&appKey=58e50827114a43dca4fe80587bcc8f37";
  
-  // jQuery.ajaxPrefilter(function (options) {
-  //   if (options.crossDomain && jQuery.support.cors) {
-  //       options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
-  //   }
-  // });
-
-  
-
   var queryURL = baseURL + carrier + "/" + flightNum + "/arr/" + date + appId + apiKey;
 
   // Functions 
-
   function runQuery(flightStats, queryURL) {
       //$.support.cors = true;
-      $.ajax({
-          url: queryURL,
-          method: "GET",
-          dataType: "jsonp",
-          xhrFields: {
-            withCredentials: true
-        },
-        
+      $.ajax({ url: queryURL, method: "GET", dataType: "jsonp", 
+          xhrFields: {withCredentials: true},
       })
-
-      .done(function(flightData) {
-          console.log(flightData);            
-      });
-
+      // .done runs if the data is retrieved successfully. 
+      .done(function() {
+                   
       // Clear Flight Search from previous run
       $('#flightSearch').empty();
 
-      // Search through Flight data
-      // Send information to HTML
-      // Check if flight exists if not return Null
-      // Attach content to appropriate row in HTML.
+      // Need to figure out how to loop through web service. 
+      for (var i = 0; i < flightStatus; i++) {
+        console.log(flightStatus[i].departureTerminal);
+        console.log(flightStatus[i].departureGate);
+      }
 
       console.log(flightStats);
       console.log(queryURL);
+      console.log(flightData);   
 
-  };
+  });
+
+}
 
   //  Return these fields to the #flightSearch table
 // requestedFields: [ 
@@ -86,35 +69,22 @@ $(".find_button").click(function() {
 
   });
 
-//Attach content to appropriate departure row.
+//Attach content to appropriate arrival row.
 // var newRow = $("<tr>").append(
-//     $("<td>").text(departure),
-//     $("<td>").text(departureDate),
-//     $("<td>").text(scheduleOfDeparture),
-//     $("<td>").text(actualDeparture),
-//     $("<td>").text(terminal),
-//     $("<td>").text(gate),
-// );
+//   $("<td>").text(airportCode),
+//   $("<td>").text(scheduledTime),
+//   $("<td>").text(scheduledDate),
+//   $("<td>").text(actualDate),
+//   $("<td>").text(terminal),
+//   $("<td>").text(gate),
+// )
 
   // $("#flightSearch").append(newRow);
-
-  // $("#contentArea").prepend(imageDiv);
-
-
-// Attach content to appropriate Departure Row.
-
-    // $("#arrival").val("");
-    // $("#arrivalDate").val("");
-    // $("#scheduledArrival").val("");
-    // $("#estimatedArrival").val("");
-    // $("#gate").val("");
-
-
-// $("#flightSearch").append(newRow);
-
   // $("#contentArea").prepend(imageDiv);
 
 
 
 
+
+  
 
